51fb68d1fe0c76c792b2ecc4d27e1ec2c11c3c09
wur3 <wu.richard.98@gmail.com>
2021-12-07 23:03:50 -0500

    tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,14 +6,17 @@
  */
 
 import * as React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  const data: any = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -23,9 +26,11 @@ const Layout = ({ children }) => {
     }
   `)
 
+  const siteTitle: string = data.site.siteMetadata?.title || `Title`
+
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle={siteTitle} />
       <div
         style={{
           margin: `0 auto`,
@@ -46,10 +51,6 @@ const Layout = ({ children }) => {
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
