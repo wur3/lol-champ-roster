f9c4e442c89f5b8aa062fd7247309f27bbc2c181
wur3 <wu.richard.98@gmail.com>
2021-12-10 00:26:40 -0500

    no header for now

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -34,8 +34,7 @@ const Layout = ({ children }: LayoutProps) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: `0 auto`,
         }}
       >
         <main>{children}</main>
