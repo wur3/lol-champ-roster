4411a3ef7be33bd3050c0eb313cb76eb65d3f97c
wur3 <wu.richard.98@gmail.com>
2021-12-11 22:02:54 -0500

    dont ddos

--- src/components/ChampList.tsx
+++ src/components/ChampList.tsx
@@ -1,22 +1,28 @@
 import * as React from 'react';
-import axios from 'axios';
+import axios, { AxiosInstance} from 'axios';
 import './ChampList.css';
 import ListItem from './ListItem';
 import { Champion } from './ChampionInterface';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { setupCache } from 'axios-cache-adapter';
+
+interface ChampListProps {
+  api: AxiosInstance
+}
 
 const ChampList = () => {
   const [champs, setChamps] = useState([] as Champion[]);
-
-  axios.get('https://ddragon.leagueoflegends.com/api/versions.json')
+  useEffect(() => {
+    axios.get('https://ddragon.leagueoflegends.com/api/versions.json')
     .then(res => {
       const current_ver = res.data[0]
       axios.get(`https://ddragon.leagueoflegends.com/cdn/${current_ver}/data/en_US/champion.json`)
         .then(res => {
           const champions: Champion[] = Object.values(res.data.data)
           setChamps(champions);
-      })
-  });
+        })
+    });
+  }, [])
 
   const champList = champs.sort((a,b)=>a['name'].localeCompare(b['name'])).map(c => 
     <ListItem champ={c}/>
