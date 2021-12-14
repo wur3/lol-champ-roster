4511c0a8c5277d463b4bc7f2b0232814275fd06b
wur3 <wu.richard.98@gmail.com>
2021-12-14 00:21:33 -0500

    Revert "use list background in 404? if no where else..." bc tests HATE him
    This reverts commit c12f4ee0dbe4178e2cb64dcca0b15fe17573f8b5.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,9 @@
 import { PageProps } from "gatsby"
 import * as React from "react"
 import Layout from "../components/layout"
-import ListBackground from "../components/ListBackground"
 
 const Error404Page: React.FC<PageProps> = () => (
   <Layout>
-    <ListBackground />
     <h1>You are here!</h1>
     <h2>But nothing found for you #404</h2>
   </Layout>
