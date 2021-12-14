c12f4ee0dbe4178e2cb64dcca0b15fe17573f8b5
wur3 <wu.richard.98@gmail.com>
2021-12-14 00:17:36 -0500

    use list background in 404? if no where else...

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,11 @@
 import { PageProps } from "gatsby"
 import * as React from "react"
 import Layout from "../components/layout"
+import ListBackground from "../components/ListBackground"
 
 const Error404Page: React.FC<PageProps> = () => (
   <Layout>
+    <ListBackground />
     <h1>You are here!</h1>
     <h2>But nothing found for you #404</h2>
   </Layout>
